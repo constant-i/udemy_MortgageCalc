ebfd9abcca84b7ff5db53a12415ea360bcfef5d9
constant-i <constant-i@yandex.ru>
2019-03-24 17:42:07 +0300

    рефакторинг: логику расчёта и печати перенёс в отдельный метод

--- MortgageCalc.java
+++ MortgageCalc.java
@@ -5,15 +5,11 @@ public class MortgageCalc {
 
     public static void main(String[] args) {
 
-        System.out.println("для начала работы введите через пробел срок кредитования, сумму, процент, " +
-                "суммарный доход, количество членов семьи, [опционально планируемая доп. плата]");
+        System.out.println("для начала работы введите через пробел срок кредитования, сумму, процент, суммарный доход, количество членов семьи, [опционально планируемая доп. плата]");
         final int SHORT_LENGTH = 5;
         final int HEIGHT_LENGTH = 6;
 
         if (args.length >= SHORT_LENGTH && args.length <= HEIGHT_LENGTH) {
-            int currentMonth = 0;
-
-            Calculator calc;
 
             int years = Integer.parseInt(args[0]);
             float loanAmount = Float.parseFloat(args[1]);
@@ -31,53 +27,8 @@ public class MortgageCalc {
                             "members = %2d, extra = %10.2f %n",
                     years, loanAmount, interestRate, summaryIncome, familyMembers, extraMonthlyPayment);
 
-            calc = new Calculator(years, loanAmount, interestRate, extraMonthlyPayment);
-//                System.out.println("Введите количество членов семьи (от 1 до 5)");
-//                Scanner sc = new Scanner(System.in);
-//                familyMembers = sc.nextInt();
-//                System.out.println("членов семьи = " + familyMembers);
-//                System.out.println("Введите суммарный доход семьи");
-//                summaryIncome = sc.nextFloat();
-//                System.out.println("доход = " + summaryIncome);
-//                int years = 10;
-//                float loanAmount = 3000000;
-//                float interestRate = 10;
-//                float extraMonthlyPayment = 0F;
-//                calc = new Calculator(years, loanAmount, interestRate);
-
-            float pmnt = calc.Payment();
-            if (((summaryIncome * 0.5 > pmnt) && (familyMembers == 1)) ||
-                    ((summaryIncome * 0.45 > pmnt) && (familyMembers == 2)) ||
-                    ((summaryIncome * 0.35 > pmnt) && (familyMembers == 3)) ||
-                    ((summaryIncome * 0.30 > pmnt) && (familyMembers == 4)) ||
-                    ((summaryIncome * 0.25 > pmnt) && (familyMembers == 5))) {
-
-                System.out.println("MONTH, STARING_BALANCE, PAYMENT,     INTEREST,  PRINCIPAL,   ENDING_BALANCE, TOTAL_INTEREST");
-                do {
-                    currentMonth++;
-                    float payment = calc.Payment() + calc.getExtraMonthlyPayment();
-                    float stBal = calc.getEndingBalance();
-                    calc.setStaringBalance(stBal);
-                    float endBal = calc.EndingBalance();
-                    calc.setEndingBalance(endBal);
-                    float principal = calc.Principal();
-                    float interest = calc.Interest();
-                    float totInterest = calc.getTotalInterest() + interest;
-                    calc.setTotalInterest(totInterest);
-                    if (stBal < payment) {
-                        payment = principal = stBal;
-                        calc.setEndingBalance(-0.0001F);
-                        endBal = calc.getEndingBalance();
-                    }
-                    System.out.printf("%3d; %13.2f; %14.2f; %10.2f; %10.2f; %12.2f; %12.2f%n",
-                            currentMonth, stBal, payment, interest, principal, endBal, totInterest);
-                } while ((calc.getEndingBalance() >= 0) /*&& currentMonth < calc.getMonth()*/);
-
-            } else {
-                System.out.printf("К нашему сожалению кредит на %10.2f руб." +
-                        "при доходе %9.2f руб. на срок %3d лет " +
-                        "и ежемесечном платеже %8.2f руб. НЕВОЗМОЖЕН :(", loanAmount, summaryIncome, years, pmnt);
-            }
+            Calculator calc = new Calculator(years, loanAmount, interestRate, extraMonthlyPayment);
+            calc.PrintMortgage(summaryIncome, familyMembers);
         }
     }
 }
